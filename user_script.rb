264305b082153b4bbaf850744e414ecc146ec91c
Yeni <yenicapote08@gmail.com>
2016-08-25 15:12:13 -0700

    improved user script

--- user_script.rb
+++ user_script.rb
@@ -1,9 +1,18 @@
 
 require_relative 'account'
 
-id = rand(111111..999999)
-lucy = Bank::Account.new("Lucy", id, 235.00)
-puts lucy.initial_bal
-puts lucy.deposit(25)
-puts lucy.withdraw(240.00)
-puts lucy.balance
+
+# all_accounts = Bank::Account.all
+# p all_accounts
+# puts all_accounts[0].id
+# puts lucy.initial_bal
+# puts lucy.deposit(25)
+# puts lucy.withdraw(13240.00)
+# puts lucy.balance
+# puts all_accounts.find(15156)
+# p all_accounts[1214]
+look = Bank::Account.find(15152)
+
+p look
+
+p look.balance
